7f2821e335f189f7afcb965a32e6b9b8b054560a
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-08-25 12:33:50 +0000

    Hotel

--- Client/src/Component/Rating/Rating.jsx
+++ Client/src/Component/Rating/Rating.jsx
@@ -74,7 +74,7 @@ function Rating() {
                         Add Your Rating
                       </h1>
                     </div>
-                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                    <div className="lg:w-1/2 md:w-2/3 mx-auto ">
                       <div className="flex flex-wrap -m-2">
                         <div className="p-2 w-full">
                           <div className="relative">
