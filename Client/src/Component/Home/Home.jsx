352bfe69e5e009b007e9fd354fadfb6b5c97126b
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-08-29 20:51:57 +0530

    Hotel

--- Client/src/Component/Home/Home.jsx
+++ Client/src/Component/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
   // for auth user
   useEffect(() => {
     axios
-      .get(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}user`, {
+      .get(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/user`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
