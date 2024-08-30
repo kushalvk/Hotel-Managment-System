680092cb4f2a9a96078285bf53057582a11d8dd0
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-08-30 12:11:03 +0000

    Hotel

--- Client/src/Component/Header/Header.jsx
+++ Client/src/Component/Header/Header.jsx
@@ -12,6 +12,9 @@ function Header() {
   function signupbtn() {
     navigate("/signup");
   }
+  function facilitiesbtn() {
+    navigate("/facilities");
+  }
 
   // usedata -> for auth user
   const [userData, setUserData] = useState(null);
@@ -40,9 +43,9 @@ function Header() {
           <a href="/" className="mr-5 text-white hover:text-gray-400">
             Home
           </a>
-          <a href="facilities" className="mr-5 text-white hover:text-gray-400">
+          <button onClick={facilitiesbtn} className="mr-5 text-white hover:text-gray-400">
             Facilities
-          </a>
+          </button>
           <a href="contactas" className="mr-5 text-white hover:text-gray-400">
             Contact as
           </a>
