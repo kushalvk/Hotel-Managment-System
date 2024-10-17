5cf56f482f52c91544db7c7b81efc00c39e6b110
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-10-17 13:24:15 +0530

    Update index.js
    Front-end URL

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ const paymentRouter = require("./routers/payment_router");
 
 const app = express();
 const corsOptions = {
-  origin: ['https://solid-space-adventure-5gq5457g7jv7c7xxj-5173.app.github.dev'], // Your frontend origin
+  origin: ['https://hotel-managment-system-server.vercel.app/'], // Your frontend origin
   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Auth-Token', 'Origin'],
