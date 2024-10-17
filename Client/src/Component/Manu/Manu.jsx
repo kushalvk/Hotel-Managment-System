0ce3485e68c37def57501b3eb19cb54101062c5a
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-10-17 12:53:55 +0000

    manu

--- Client/src/Component/Manu/Manu.jsx
+++ Client/src/Component/Manu/Manu.jsx
@@ -8,26 +8,31 @@ function MenuPage() {
         {
             title: 'Breakfast',
             description: 'Start your day with our nutritious breakfast options, including fresh juices, cereals, and more.',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkJaJ1fm3TlKSbG8e84BHmjO018HgsoJ1BbQ&s',
             price: '₹250',
         },
         {
             title: 'Lunch',
             description: 'Enjoy our delicious lunch specials, including soups, sandwiches, and hearty meals.',
+            img: 'https://content.jdmagicbox.com/comp/def_content_category/inexpensive-indian-restaurants-below-rs-500-/360-f-361869194-7jgmiosj2iuni0ayovhvyhkvan6pkoah-inexpensive-indian-restaurants-below-rs-500--1-quqo2.jpg',
             price: '₹500',
         },
         {
             title: 'Dinner',
             description: 'Indulge in a variety of fine dining options from pasta to grilled delicacies.',
+            img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b0/fe/6b/aloft-rohit-dassani.jpg?w=600&h=-1&s=1',
             price: '₹750',
         },
         {
             title: 'Desserts',
             description: 'End your meal with our sweet and savory desserts, including cakes and pastries.',
+            img: 'https://c8.alamy.com/comp/AWT3TA/gourmet-desserts-served-at-exclusive-five-star-restaurant-in-new-york-AWT3TA.jpg',
             price: '₹200',
         },
         {
             title: 'Beverages',
             description: 'Choose from a wide selection of refreshing drinks including teas, coffees, and juices.',
+            img: 'https://images.immediate.co.uk/production/volatile/sites/30/2021/11/Grenadine-diabolo-05c968f.jpg',
             price: '₹100',
         },
     ];
@@ -54,7 +59,7 @@ function MenuPage() {
                                 <img src={item.img} alt={item.title} className="w-full h-96 object-cover rounded-lg shadow-lg" />
                                 <h2 className="text-2xl font-semibold text-white mt-4">{item.title}</h2>
                                 <p className="text-white mt-2">{item.description}</p>
-                                <p className="text-white mt-2">{item.price}</p>
+                                <p className="text-white mt-2">₹ {item.price}</p>
                             </div>
                         ))}
                     </div>
