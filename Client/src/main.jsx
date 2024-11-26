b1cd6cd1084fee16bb4c6e1ce8989d4d9e0473ad
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-11-26 17:22:08 +0000

    Solve Error: Change Path in main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Login from './Components/Pages/Login/Login.jsx'
 import SignUp from './Components/Pages/Sign Up/SignUp.jsx'
-import Home from './Components/Pages/Home/Home.jsx'
+import DefaultHome from './Components/Pages/Home/DefaultHome.jsx'
 import Booking from './Components/Pages/Booking/Booking.jsx';
 import Gallery from './Components/Pages/Gallery/Gallery.jsx'
 import Policies from './Components/Pages/Policies/Policies.jsx'
@@ -32,7 +32,7 @@ import ManuPage from './Components/Pages/Manu/Manu.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<DefaultHome />} />
       <Route path='login' element={<Login />} />
       <Route path='signup' element={<SignUp />} />
       <Route path='booking' element={<Booking />} />
